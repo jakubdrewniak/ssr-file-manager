5b31c28347909421fc7a7e048d72bd324c35ecb8
jakub-drewniak <drewniak.jakub@gmail.com>
2020-03-26 17:45:21 +0100

    Routing redirect to main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router'
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/' }
+
 ];
 
 @NgModule({
